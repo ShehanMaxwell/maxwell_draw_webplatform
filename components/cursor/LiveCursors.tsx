b58ded93bc3294cbc65f5d7d6850983dc2cfad82
Shehan Maxwell <shehanbbn@gmail.com>
2024-02-17 20:16:47 +0530

    changed 5 files

---
+++ components/cursor/LiveCursors.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { LiveCursorProps } from '@/types/type';
+import Cursor from './Cursor';
+import { COLORS } from '@/constants';
+
+const LiveCursors = ({ others }: LiveCursorProps) => {
+  return (
+    <div>
+      {others.map(({ connectionId, presence }) => {
+        if (!presence?.cursor) {
+          return null;
+        }
+
+        return (
+          <Cursor
+            key={connectionId}
+            color={COLORS[Number(connectionId) % COLORS.length]}
+            x={presence.cursor.x}
+            y={presence.cursor.y}
+            message={presence.message}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default LiveCursors;

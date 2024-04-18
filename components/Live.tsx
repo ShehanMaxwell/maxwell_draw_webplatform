2f141e50484a22d3b6e16be1331f405de4a70b20
Shehan Maxwell <shehanbbn@gmail.com>
2024-04-18 17:56:44 +0530

    modified Live.tsx in components

--- components/Live.tsx
+++ components/Live.tsx
@@ -24,6 +24,12 @@ const Live = () => {
   const broadcast = useBroadcastEvent();
 
   useInterval(() => {
+    setReaction((reaction) =>
+      reaction.filter((r) => r.timestamp > Date.now() - 4000)
+    );
+  }, 1000);
+
+  useInterval(() => {
     if (
       cursorState.mode === CursorMode.Reaction &&
       cursorState.isPressed &&
